f22d1fe2f8c0b02ae3e2b79fac97006edb477656
Adoraxuu <adoraxu01@gmail.com>
2024-02-21 14:03:54 +0800

    fetch(0218.rb):新增解法

--- Ruby/0218.rb
+++ Ruby/0218.rb
@@ -6,7 +6,18 @@ def digital_root2(n)
   n.zero? ? 0 : (n - 1) % 9 + 1
 end
 
-puts digital_root(16)
-puts digital_root(942)
+def digital_root3(n)
+  case
+  when n.zero?
+    0
+  when n % 9 == 0
+    9
+  else
+    n % 9
+  end
+end
+
+puts digital_root3(16)
+puts digital_root2(942)
 puts digital_root(132189)
 puts digital_root(493193)
